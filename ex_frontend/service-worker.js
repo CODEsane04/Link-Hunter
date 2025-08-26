f9410b21ef11396fd7cb606b3ef65de38c7dc45a
CODEsane04 <debjitcodes18@gmail.com>
2025-08-26 23:28:04 +0530

    rewrote the service-worker.js, it sends the POST request to the backend, fetches the url from the backend and stores it into the chrome local storage, so that the links can be accessed by the vps.js & shown on the frontend

--- ex_frontend/service-worker.js
+++ ex_frontend/service-worker.js
@@ -10,16 +10,56 @@ chrome.runtime.onInstalled.addListener(() => {
     setupContextMenu();
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     if (info.menuItemId === "searchWithVPS") {
 
         console.log("Image right-clicked. URL : ", info.srcUrl);
 
         //storing the img url in the chrome storage as a key value pair
         const img_url = info.srcUrl;
-        chrome.storage.local.set({capturedImageUrl: img_url}, () => {
+        /*chrome.storage.local.set({capturedImageUrl: img_url}, () => {
             console.log("image url stored succesfully in chrome storage");
-        });
+        });*/
+        await getlinks(img_url);
     }
 });
+
+async function getlinks(img_url) {
+
+    const backendUrl = 'http://localhost:3000/api/get-url';
+    console.log("sending image url to the backend : ", img_url);
+    
+    try {
+        
+        //response is a Response Object, you need to unpack this object
+        //which is done by response.json() it unpacks & parses
+        const response = await fetch(backendUrl, {
+            method : 'POST',
+            headers : {
+                'content-Type' : 'application/json'
+            },
+            body : JSON.stringify( {imageUrl : img_url} ),
+        });
+
+        if (!response.ok) {
+            console.error(`HTTP error status : ${response.status}`); 
+        } else {
+            const data = await response.json();
+            console.log("recieved the links from the backend, in response to the POST request", data.tutorials);
+
+            //we have to store these links in the chrome local storage
+            chrome.storage.local.set({fetchedLinks : data.tutorials}, () => {
+                console.log("stored the fetched links successfully in the local storage");
+            })
+            
+        }
+        
+        
+    } catch (e) {
+        console.log("failed to fetch links : ", e);
+    }
+    
+}
+
+
 

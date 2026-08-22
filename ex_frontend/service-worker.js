e166477b587d1d3e453c127de115f821bc1922e2
CODEsane04 <debjitcodes18@gmail.com>
2026-08-22 14:54:54 +0530

    frontend updated with current backend-url

--- ex_frontend/service-worker.js
+++ ex_frontend/service-worker.js
@@ -26,9 +26,9 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
 
 async function getlinks(img_url) {
 
-    // backend url - https://link-hunter-ks1a.onrender.com
+    // backend url - https://link-hunter-1.onrender.com
 
-    const backendUrl = 'https://link-hunter-ks1a.onrender.com/get_links';
+    const backendUrl = 'https://link-hunter-1.onrender.com/get_links';
     console.log("sending image url to the backend : ", img_url);
     
     try {
